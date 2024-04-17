ec10d091f8907acbb1d7a61c3d99c2be58c0b3ec
Ryan Shum <ryancks09@gmail.com>
2024-04-17 18:42:53 -0400

    Initial Commit

---
+++ src/main/java/com/humber/healthcare/HealthcareApplication.java
@@ -0,0 +1,13 @@
+package com.humber.healthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthcareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/humber/healthcare/HealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber.healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
